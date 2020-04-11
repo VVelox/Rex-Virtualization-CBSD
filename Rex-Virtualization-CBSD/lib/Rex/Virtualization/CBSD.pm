3d9ac532d8b75baa7b15b7befc7f06423579089c
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2020-04-11 09:23:25 -0500

    initial stuff

---
+++ Rex-Virtualization-CBSD/lib/Rex/Virtualization/CBSD.pm
@@ -0,0 +1,109 @@
+package Rex::Virtualization::CBSD;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Rex::Virtualization::CBSD - The great new Rex::Virtualization::CBSD!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Rex::Virtualization::CBSD;
+
+    my $foo = Rex::Virtualization::CBSD->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-HAdley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-rex-virtualization-cbsd at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Rex-Virtualization-CBSD>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Rex::Virtualization::CBSD
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Rex-Virtualization-CBSD>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Rex-Virtualization-CBSD>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Rex-Virtualization-CBSD>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Rex-Virtualization-CBSD>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2020 by Zane C. Bowers-HAdley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Rex::Virtualization::CBSD
